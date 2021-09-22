997e525ad2fa0572dcb224335d17265509533d82
Jenna Hee <hee.jenna@gmail.com>
2021-09-22 12:05:07 -1000

    updating files

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,16 +1,31 @@
 function home() {
-	let html;
 	if (currentUser == "") {
 		// logged out
-		html = document.getElementById("anon-home").innerHTML;
+		let html = document.getElementById("anon-home").innerHTML;
+		render(html);
 	} else {
 		// logged in
-		let src = document.getElementById("home").innerHTML;
-		let template = Handlebars.compile(src);
-		let context = {};
-		html = template(context);
+		fetch("https://cookuni-project-default-rtdb.firebaseio.com/recipes.json")
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				// let recipeArr = Object.entries(data).map((recipe) => {
+				// 	let [id, recipeInfo] = recipe;
+				// 	recipeInfo.id = id;
+				// 	return recipeInfo;
+				// });
+				let recipesArr = Object.values(data);
+				let src = document.getElementById("home").innerHTML;
+				let template = Handlebars.compile(src);
+				let context = { recipesArr };
+				let html = template(context);
+				render(html);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	}
-	render(html);
 }
 
 function register() {
@@ -30,12 +45,20 @@ function login() {
 	render(html);
 }
 
-function view() {
-	let src = document.getElementById("view").innerHTML;
-	let template = Handlebars.compile(src);
-	let context = {};
-	let html = template(context);
-	render(html);
+function view(recipeID) {
+	fetch(`https://cookuni-project-default-rtdb.firebaseio.com/recipes/${recipeID}.json`)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			let src = document.getElementById("view").innerHTML;
+			let template = Handlebars.compile(src);
+			let html = template(data);
+			render(html);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
 function share() {
@@ -44,6 +67,79 @@ function share() {
 	let context = {};
 	let html = template(context);
 	render(html);
+
+	document.getElementById("share-btn").addEventListener("click", function (event) {
+		event.preventDefault();
+		let meal = document.getElementById("defaultRecepieShareMeal").value;
+		let ingredients = document.getElementById("defaultRecepieShareIngredients").value.split(", ");
+		let prepMethod = document.getElementById("defaultRecepieShareMethodOfPreparation").value;
+		let description = document.getElementById("defaultRecepieShareDescription").value;
+		let foodImageURL = document.getElementById("defaultRecepieShareFoodImageURL").value;
+		let category = document.getElementById("share-category");
+
+		let data = {
+			meal,
+			ingredients,
+			prepMethod,
+			description,
+			foodImageURL,
+			category: category.options[category.selectedIndex].value,
+			categoryImageURL: "",
+			likesCounter: 0,
+			creator: currentUser,
+		};
+		if (data.category == "Vegetables and legumes/beans") {
+			data.categoryImageURL = "https://cdn.pixabay.com/photo/2017/10/09/19/29/eat-2834549__340.jpg";
+		} else if (data.category == "Fruits") {
+			data.categoryImageURL = "https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029__340.jpg";
+		} else if (data.category == "Grain Food") {
+			data.categoryImageURL = "https://cdn.pixabay.com/photo/2014/12/11/02/55/corn-syrup-563796__340.jpg";
+		} else if (data.category == "Milk, cheese, eggs and alternatives") {
+			data.categoryImageURL =
+				"https://image.shutterstock.com/image-photo/assorted-dairy-products-milk-yogurt-260nw-530162824.jpg";
+		} else if (data.category == "Lean meats and poultry, fish and alternatives") {
+			data.categoryImageURL =
+				"https://t3.ftcdn.net/jpg/01/18/84/52/240_F_118845283_n9uWnb81tg8cG7Rf9y3McWT1DT1ZKTDx.jpg";
+		}
+		// console.log(data);
+
+		let url = "https://cookuni-project-default-rtdb.firebaseio.com/recipes.json";
+		let headers = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(data),
+		};
+		fetch(url, headers)
+			.then((response) => {
+				if ((response.status = 200)) {
+					return response.json();
+				} else {
+					console.log(response.status);
+				}
+			})
+			.then((data) => {
+				// console.log(data.name);
+				let patchHeaders = {
+					method: "PATCH",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ id: data.name }),
+				};
+				fetch(`https://cookuni-project-default-rtdb.firebaseio.com/recipes/${data.name}.json`, patchHeaders)
+					.then((response) => {
+						if (response.status == 200) {
+							window.location.hash = "#home";
+						} else {
+							console.log(response.status);
+						}
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	});
 }
 
 function edit() {
@@ -59,12 +155,9 @@ function render(html) {
 }
 
 function refreshNavBar() {
-	// let nav = document.getElementById("navbar");
-
 	let src = document.getElementById("navbar").innerHTML;
 	let template = Handlebars.compile(src);
 	let context = users[currentUser];
-
 	document.getElementById("nav").innerHTML = template(context);
 
 	let welcome = document.getElementById("nav-welcome");
@@ -74,30 +167,29 @@ function refreshNavBar() {
 	let register = document.getElementById("nav-register");
 
 	if (currentUser == "") {
-		// let html = document.getElementById("anon-nav").innerHTML;
-		// nav.innerHTML = html;
-
 		welcome.style.display = "none";
 		share.style.display = "none";
 		logout.style.display = "none";
-		// login.removeAttribute("style");
-		// register.removeAttribute("style");
-		login.style.display = "block";
-		register.style.display = "block";
-	} else {
-		// let src = document.getElementById("active-nav").innerHTML;
-		// let template = Handlebars.compile(src);
-		// let context = users[currentUser];
-		// console.log(context);
-		// nav.innerHTML = template(context);
+		login.removeAttribute("style");
+		register.removeAttribute("style");
 
+		if (
+			hashRoute == "#share" ||
+			hashRoute == "logout" ||
+			hashRoute.includes("#edit") ||
+			hashRoute.includes("#view")
+		) {
+			window.location.hash = "#home";
+		}
+	} else {
 		welcome.removeAttribute("style");
 		share.removeAttribute("style");
 		logout.removeAttribute("style");
-		// welcome.style.display = "block";
-		// share.style.display = "block";
-		// logout.style.display = "block";
 		login.style.display = "none";
 		register.style.display = "none";
+
+		if (hashRoute == "#login" || hashRoute == "register") {
+			window.location.hash = "#home";
+		}
 	}
 }
--- scripts/server.js
+++ scripts/server.js
@@ -15,6 +15,31 @@ let users = {
 		username: "booster69",
 	},
 };
+let recipes = {
+	"-Mk57i9lfZ9_qNbg_2xs": {
+		category: "Grain Food",
+		categoryImageURL: "https://cdn.pixabay.com/photo/2014/12/11/02/55/corn-syrup-563796__340.jpg",
+		creator: "-Mk5lQKNPeI65Eh38RXq",
+		description: "Test description",
+		foodImageURL:
+			"https://images.fineartamerica.com/images/artworkimages/mediumlarge/3/1-no-one-needs-to-know-pineapple-pizza-pineapple-on-pizza-yestic.jpg",
+		ingredients: ["Pizza Dough", "Mozzarella Cheese", "Pizza Sauce"],
+		likesCounter: 0,
+		meal: "Hawaiian Pizza",
+		prepMethod: "Make the pizza.",
+	},
+	"-Mk584f6FVl6MqxCTAcn": {
+		category: "Fruits",
+		categoryImageURL: "https://cdn.pixabay.com/photo/2017/06/02/18/24/fruit-2367029__340.jpg",
+		creator: "-Mk53Gm87wJcT9rmRHBM",
+		description: "Fruit salad, yummy yummy",
+		foodImageURL: "https://fitfoodiefinds.com/wp-content/uploads/2020/05/salad-1-768x1152.jpg",
+		ingredients: ["Melon", "Apple", "Strawberry"],
+		likesCounter: "0",
+		meal: "Fruit Salad",
+		prepMethod: "Beat your dog",
+	},
+};
 
 let currentUser = "-Mk5lQKNPeI65Eh38RXq";
 let active = false;
@@ -32,8 +57,11 @@ function listen() {
 			register();
 		} else if (hashRoute == "#login") {
 			login();
-		} else if (hashRoute == "#login") {
-			login();
+		} else if (hashRoute == "#share") {
+			share();
+		} else if (hashRoute.includes("view")) {
+			let [, recipeID] = hashRoute.split("/");
+			view(recipeID);
 		}
 	}
 	setTimeout(listen, 200);
